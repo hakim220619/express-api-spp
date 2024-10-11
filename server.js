2e3a6003e1ce3210edf927b4b93aaacf38b5b910
Dani lukman <danilukman2206@gmail.com>
2024-10-11 21:37:53 +0700

    asd

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
 });
 
 // Set port and listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });

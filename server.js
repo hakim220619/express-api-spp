5b99d366187ef0614ad1fb20a2631fbcbe204273
Dani lukman <danilukman2206@gmail.com>
2024-10-20 19:31:43 +0700

    asd

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // const bodyParser = require("body-parser"); /* deprecated */
 const cors = require('cors');
+const path = require('path');
 
 
 const app = express();

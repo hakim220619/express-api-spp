9ce3f68bee6baea62a382c31a4a9e575a12eb682
Dani lukman <danilukman2206@gmail.com>
2024-10-21 09:16:33 +0700

    asd

--- server.js
+++ server.js
@@ -2,40 +2,27 @@ const express = require("express");
 // const bodyParser = require("body-parser"); /* deprecated */
 const cors = require('cors');
 const path = require('path');
-const bodyParser = require("body-parser");
+
 
 const app = express();
 // Serve static files from the 'uploads' directory
-app.use(cors({
-  origin: '*', // Atur sesuai kebutuhan, bisa spesifik domain.
-  methods: ['POST', 'GET', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(200);
-});
-
-app.use('/uploads', express.static('uploads'));
 
 // parse requests of content-type - application/json
+app.use(express.json()); /* bodyParser.json() is deprecated */
 
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Api DLH." });
+  res.json({ message: "Welcome to bezkoder application." });
 });
 
 require("./app/routes/routes.js")(app);

cb10bedb16ba02ab03cf2a9c1816940d6381f131
Dani lukman <danilukman2206@gmail.com>
2024-10-21 12:31:38 +0700

    asd

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Mengaktifkan CORS untuk semua origin
-app.use(cors());
+// Konfigurasi CORS untuk mengizinkan permintaan dari semua origin
+app.use(cors({
+  origin: 'https://ypphbanjarbaru.sppapp.com', // Atau ganti dengan origin spesifik seperti 'https://your-frontend.com'
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // Jika perlu mengirim cookie atau header otentikasi
+}));
 
 // Menyajikan file statis dari folder 'uploads'
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
